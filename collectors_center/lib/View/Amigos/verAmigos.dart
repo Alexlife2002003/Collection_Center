c617157f94aaf0dd722284e7b913cb9e11d9b1ed
Alexia Hernandez Martinez <20200992@uaz.edu.mx>
2023-11-15 09:00:46 -0600

    RU7

--- collectors_center/lib/View/Amigos/verAmigos.dart
+++ collectors_center/lib/View/Amigos/verAmigos.dart
@@ -16,8 +16,135 @@ class _VerAmigosState extends State<VerAmigos> {
   List<String> amigos = [];
   String userInput = '';
 
-  void enviarSolicitud() {
-    // ... (rest of the code remains the same)
+void enviarSolicitud() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: brown,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          title: Text(
+            'Inserta el nombre del usuario que quieres añadir a amigos',
+            style: TextStyle(color: myColor),
+          ),
+          content: TextField(
+            onChanged: (value) {
+              setState(() {
+                userInput = value;
+              });
+            },
+            decoration: InputDecoration(
+              fillColor: peach,
+              filled: true,
+              hintText: 'Nombre de Usuario',
+              hintStyle: const TextStyle(fontWeight: FontWeight.bold),
+              alignLabelWithHint: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                  child: Text('Cancelar'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    sendSolicitud(userInput.trim()).then((int cumplido) {
+                      Navigator.of(context).pop();
+                      String message = "";
+                      switch (cumplido) {
+                        case 0:
+                          message =
+                              "No se puede enviar solicitud porque ya es tu amigo";
+                          break;
+                        case 1:
+                          message = "Ya le enviaste solicitud";
+                          break;
+                        case 2:
+                          message = "No te puedes enviar solicitud a ti mismo";
+                          break;
+                        case 3:
+                          message = "Error al enviar la solicitud";
+                          break;
+                        case 4:
+                          message = "Usuario no existe";
+                        case 10:
+                          message = userInput;
+                          break;
+                      }
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            icon: cumplido == 10
+                                ? Icon(
+                                    Icons.check_circle_outline,
+                                    color: green,
+                                    size: 40,
+                                  )
+                                : Icon(
+                                    Icons.error,
+                                    color: red,
+                                    size: 40,
+                                  ),
+                            backgroundColor: brown,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            title: Text(
+                              cumplido == 10 ? 'Solicitud enviada a' : message,
+                              style: TextStyle(
+                                color: cumplido == 10 ? myColor : red,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            content: Text(
+                              cumplido == 10 ? userInput : '',
+                              style: TextStyle(color: peach),
+                              textAlign: TextAlign.center,
+                            ),
+                            actions: <Widget>[
+                              Center(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    userInput = "";
+                                    Navigator.of(context).pop();
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.blue,
+                                  ),
+                                  child: Text('OK'),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                  child: Text('Enviar'),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
