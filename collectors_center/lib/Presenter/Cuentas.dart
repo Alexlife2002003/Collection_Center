7f22107a3fe56fb8a7fb8137e16cb5faa33386cd
IsaacRivCor <89028958+IsaacRivCor@users.noreply.github.com>
2023-11-22 18:59:48 -0600

    Update Cuentas.dart

--- collectors_center/lib/Presenter/Cuentas.dart
+++ collectors_center/lib/Presenter/Cuentas.dart
@@ -16,9 +16,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-//////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 //  Navegacion dentro de la app encargada de acciones de registro e inicio de sesion del usuarios//
-//////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Función encargada de la eliminación de la cuenta
 Future<void> eliminarCuenta(BuildContext context) async {
@@ -96,7 +96,6 @@ Future<void> eliminarCuenta(BuildContext context) async {
       await user.delete();
 
       // Show success message
-
       showSnackbar(context, "Se ha borrado la cuenta", green);
     }
   } catch (e, stackTrace) {
